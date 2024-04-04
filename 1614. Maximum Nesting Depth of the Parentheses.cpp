52eea0c5612c25acf827651354da6334a396f02c
Aditya Agrawal <agr2003aditya@gmail.com>
2024-04-04 19:15:12 +0530

    Solution of 04-04-2024

---
+++ 1614. Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans=0, cnt=0;
+        for(char ch : s){
+            if(ch == '(')
+                cnt++;
+            else if(ch == ')')
+                cnt--;
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
