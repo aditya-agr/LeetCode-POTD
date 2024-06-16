f766606b9a9f704cecfa0cbd0134a5bb88709ce8
Aditya Agrawal <agr2003aditya@gmail.com>
2024-06-16 15:15:28 +0530

    Solution of 16-06-2024

---
+++ 330. Patching Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long maxReach=0, patch=0, i=0;
+
+       while(maxReach < n) {
+            if(i < nums.size() && nums[i] <= maxReach+1) {
+               maxReach += nums[i];
+               i++;
+            } else {
+               maxReach += (maxReach + 1);
+               patch++;
+            }
+        }
+        return patch;
+    }
+};
