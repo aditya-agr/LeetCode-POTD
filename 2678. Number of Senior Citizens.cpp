9c26f411724d243e1fc71d8b3bbe0401dba4fec2
Aditya Agrawal <agr2003aditya@gmail.com>
2024-08-01 16:43:18 +0530

    Solution of 01-08-2024

---
+++ 2678. Number of Senior Citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt=0;
+        for(string s: details){
+            if(s[11]>'6' || (s[11]=='6' && s[12]>'0'))
+                cnt++;
+        }
+        return cnt;
+    }
+};
