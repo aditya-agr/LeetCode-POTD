6a30a02fdf0b3ad24df243048443007cf5d979a3
Aditya Agrawal <agr2003aditya@gmail.com>
2025-07-06 23:45:17 +0530

    Solution of 06-07-2025

---
+++ 1865. Finding Pairs With a Certain Sum.cpp
@@ -0,0 +1,33 @@
+class FindSumPairs {
+public:
+    vector<int> n1;
+    vector<int> n2;
+    unordered_map<int, int> mp;
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        n1 = nums1;
+        n2 = nums2;
+        for(int &x : n2)
+            mp[x]++;
+    }
+    
+    void add(int index, int val) {
+        mp[n2[index]]--;
+        n2[index] += val;
+        mp[n2[index]]++;
+    }
+    
+    int count(int tot) {
+        int res = 0;
+        for(int &x : n1)
+            if(mp.count(tot-x))
+                res += mp[tot-x];
+        return res;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
