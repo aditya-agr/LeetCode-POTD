2516a9a7b27598ddd33d9ef68a6f18acb3b3281b
Aditya Agrawal <agr2003aditya@gmail.com>
2025-09-02 23:14:56 +0530

    Solution of 02-09-2025

---
+++ 3025. Find the Number of Ways to Place People I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        auto comp = [](vector<int> &a, vector<int> &b){
+            if(a[0] == b[0])
+                return a[1] < b[1];
+            return a[0] > b[0];
+        };
+        sort(points.begin(), points.end(), comp);
+        int n = points.size();
+        int cnt = 0;
+        for(int i=0; i<n-1; i++){
+            int ymx = INT_MAX;
+            for(int j=i+1; j<n; j++){
+                if(points[i][1] <= points[j][1] && ymx > points[j][1]){
+                    cnt++;
+                    ymx = points[j][1];
+                }
+            }
+        }
+        return cnt;
+    }
+};
