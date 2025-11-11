a583dfd3f74d36565982d0137ae738a08660c20e
Aditya Agrawal <agr2003aditya@gmail.com>
2025-11-11 18:54:25 +0530

    Solution of 11-11-2025

---
+++ 474. Ones and Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+
+        for(string s : strs){
+            int c0 = count(s.begin(), s.end(), '0');
+            int c1 = s.length() - c0;
+            
+            for(int i=m; i>=c0; i--)
+                for(int j=n; j>=c1; j--)
+                    dp[i][j] = max(dp[i][j], dp[i-c0][j-c1]+1);
+        }
+        return dp[m][n];
+    }
+};
