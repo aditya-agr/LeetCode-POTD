2f967f71b553286bd981c1b1741791ffa34d5649
Aditya Agrawal <agr2003aditya@gmail.com>
2025-12-08 22:12:53 +0530

    Solution of 08-12-2025

---
+++ 1925. Count Square Sum Triples.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int cnt = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                int k2 = i*i + j*j;
+                int k = sqrt(k2);
+                if(k*k == k2 && k<=n)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
