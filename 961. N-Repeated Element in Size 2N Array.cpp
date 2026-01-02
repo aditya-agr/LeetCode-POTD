15674ad17d17167021b741080b9b2a872cb4de68
Aditya Agrawal <agr2003aditya@gmail.com>
2026-01-02 20:40:57 +0530

    Solution of 02-01-2026

---
+++ 961. N-Repeated Element in Size 2N Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int x : nums){
+            mp[x]++;
+            if(mp[x] > 1)
+                return x;
+        }
+        return -1;
+    }
+};
