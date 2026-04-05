91064df3728bcf6d25c2c13dc1bee75845fb8420
Aditya Agrawal <agr2003aditya@gmail.com>
2026-04-05 13:44:30 +0530

    Solution of 05-04-2026

---
+++ 657. Robot Return to Origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int h=0, v=0;
+        for(char &c : moves){
+            if(c == 'U') v++;
+            if(c == 'D') v--;
+            if(c == 'L') h++;
+            if(c == 'R') h--;
+        }
+        return h==0 && v==0;
+    }
+};
